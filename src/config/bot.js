7f36eabeda6ed9cdf02ec17ba92c8f7882cecd8a
MOZU1412 <maelparnassebraga@gmail.com>
2026-07-08 19:40:22 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "WLLG HELPER",
         // Activity type number (0 = Playing).
         type: 0,
       },
